42b678878f92a9de4b4addb30e5e01556e0df615
nguyenphucminh <59383987+nguyenphucminh@users.noreply.github.com>
2023-02-28 20:46:20 +0700

    feat: add function get with inventory routes

--- Routes/InventoryRoutes.js
+++ Routes/InventoryRoutes.js
@@ -7,32 +7,6 @@ const inventoryRoutes = express.Router();
 import mongoose from 'mongoose';
 
 inventoryRoutes.get("/",
-    asyncHandler(async (req, res) => {
-        // const pageSize = 9;
-        // const currentPage = Number(req.query.pageNumber) || 1;
-    const keyword = req.query.keyword && req.query.keyword !== ' ' ? {
-          name: {
-              $regex: req.query.keyword,
-              $options: "i"
-          },
-          
-      } : {}
-        // const count = await Inventory.countDocuments({...keyword});
-        const inventories = await Inventory.find({...keyword}).sort({ _id: -1 })
-        // .limit(pageSize)
-        // .skip(pageSize * (currentPage - 1))
-
-        // const totalPage = [];
-        // for(let i = 1; i <= Math.ceil(count / pageSize); i++){
-        //   totalPage.push(i)
-        // }
-        // res.json({ providers, currentPage, totalPage });
-        res.json(inventories);
-
-    })
-)
-
-inventoryRoutes.get("/allinventory",
   asyncHandler(async (req, res)=>{
       await Inventory.aggregate([
         {
@@ -43,31 +17,53 @@ inventoryRoutes.get("/allinventory",
           }
         },
         {
-            $lookup: {
-              from: 'products',
-              let: { productId: "$_id" },
-              pipeline: [
-                { $match: { $expr: { $eq: ["$_id", "$$productId"] } } },
-                { $project: { name: 1 } }
-              ],
-              as: 'product'
-            }
+          $lookup: {
+            from: 'products',
+            let: { productId: "$_id" },
+            pipeline: [
+              { $match: { $expr: { $eq: ["$_id", "$$productId"] } } },
+              { $project: { name: 1, image: 1, unit: 1, category: 1, categoryDrug: 1 } },
+              {
+                $lookup: {
+                  from: "categories",
+                  localField: "category",
+                  foreignField: "_id",
+                  as: "category"
+                }
+              },
+              {
+                $lookup: {
+                  from: "categorydrugs",
+                  localField: "categoryDrug",
+                  foreignField: "_id",
+                  as: "categoryDrug"
+                }
+              },
+            ],
+            as: 'product'
+          }
         },
         { $unwind: "$product" },
         {
           $project: {
             _id: 0,
             idDrug: "$_id",
-            name: "$product" ,
+            name: "$product.name" ,
+            category: "$product.category.name",
+            categoryDrug: "$product.categoryDrug.name",
             total_count: 1,
             products: 1,
           }
-        }
+        },
       ], function(err, result) {
         if (err) {
             return res.status(404).json({ message: err });
         } else {
-          res.json(result);
+          const keyword = req.query.keyword ? req.query.keyword : ''
+          const filteredResult = result.filter(item => {
+            return item.name.includes(keyword);
+          });
+          res.json(filteredResult);
         }
       });
   })
@@ -95,10 +91,37 @@ inventoryRoutes.get("/:id",
             { $project: { 
                 _id: 0,
                 importedAt: "$importstocks.importedAt" 
-            } }
+            }}
+
           ]);
           res.json({ ...datas })
     })
 )
 
 export default inventoryRoutes
+
+// inventoryRoutes.get("/",
+//     asyncHandler(async (req, res) => {
+//         const pageSize = 9;
+//         const currentPage = Number(req.query.pageNumber) || 1;
+//     const keyword = req.query.keyword && req.query.keyword !== ' ' ? {
+//           name: {
+//               $regex: req.query.keyword,
+//               $options: "i"
+//           },
+          
+//       } : {}
+//         const count = await Inventory.countDocuments({...keyword});
+//         const inventories = await Inventory.find({...keyword}).sort({ _id: -1 })
+//         .limit(pageSize)
+//         .skip(pageSize * (currentPage - 1))
+
+//         const totalPage = [];
+//         for(let i = 1; i <= Math.ceil(count / pageSize); i++){
+//           totalPage.push(i)
+//         }
+//         res.json({ providers, currentPage, totalPage });
+//         res.json(inventories);
+
+//     })
+// )
